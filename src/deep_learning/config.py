67c6c79efd5946e5bc4b360c2bae279c3618361c
Luana Rodrigues Borges <luana.borges1@ufu.br>
2026-09-17 13:09:40 -0300

    Refactor deep learning config parameters
    Updated configuration parameters for dataset and training.

--- src/deep_learning/config.py
+++ src/deep_learning/config.py
@@ -1,12 +1,12 @@
-from pathlib import Path
-import torch
+from pathlib import Path # Para trabalhar com os caminhos
+import torch # Para verificar se tem GPU
 
 # ============================================================
 # Diretórios do Projeto
 # ============================================================
 
 # Pasta raiz do projeto
-PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
+PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent # .parent sobe uma pasta
 
 SRC_DIR = PROJECT_ROOT / "src"
 
@@ -19,26 +19,15 @@ RESULTS_DIR = PROJECT_ROOT / "results"
 # ============================================================
 # Dataset
 # ============================================================
-TOP_K_NORMALIZATIONS = 3
+TOP_K_NORMALIZATIONS = 3 # Selecionamos as 3 melhores para o exp 2
 
 RAW_DATASET_DIR = DATA_DIR / "raw"
 
 PROCESSED_DATASET_DIR = DATA_DIR / "processed"
     
-# ============================================================
-# Data Augmentation
-# ============================================================
-
-USE_AUGMENTATION = True
-
-# ============================================================
-# Modelo
-# ============================================================
-
-MODEL_NAME = "efficientnet_b0"
-
 NUM_CLASSES = 4
 
+# Importante para a indexação das classes
 CLASS_NAMES = [
     "healthy",
     "mild",
@@ -48,23 +37,24 @@ CLASS_NAMES = [
 
 IMAGE_SIZE = 224
 
-PRETRAINED = True
+PRETRAINED = True # Se vai ser pre treinado no ImageNet
 
 # ============================================================
 # Treinamento
 # ============================================================
 
-BATCH_SIZE = 32
+BATCH_SIZE = 32 # A rede recebe 32 imagens por vez
+# Se são 320 imagens -> 10 baches = 1 época
 
-NUM_WORKERS = 4
+NUM_WORKERS = 4 # Quantos processos auxiliares do DataLoader prepararão as imagens
 
-EPOCHS = 50
+EPOCHS = 50 # limite máximo
 
-LEARNING_RATE = 1e-4
+LEARNING_RATE = 1e-4 # controla o tamanho das atualizações dos pesos da rede
 
-WEIGHT_DECAY = 1e-4
+WEIGHT_DECAY = 1e-4 # evitar que os pesos cresçam excessivamente
 
-PATIENCE = 8
+PATIENCE = 8 # Early Stopping - se a validação não melhorar por 8 épocas
 
 # ============================================================
 # Divisão do Dataset
@@ -80,7 +70,9 @@ TEST_SIZE = 0.15
 # Reprodutibilidade
 # ============================================================
 
-SEEDS = [42, 68, 93]
+SPLIT_SEED = 42
+SEEDS = [42, 68, 93] # Para fazer média e desvio padrão depois
+# Não é cross - validation! Usamos os mesmos train, validation e test
 
 # ============================================================
 # Hardware
