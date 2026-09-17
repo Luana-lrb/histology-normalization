6fcccfde31340a6ed22dbeee8deb3b674fde69fa
Luana Rodrigues Borges <luana.borges1@ufu.br>
2026-09-17 13:13:03 -0300

    Refactor create_model function with comments in Portuguese
    Removed comments and added explanations in Portuguese for model creation.

--- src/deep_learning/model.py
+++ src/deep_learning/model.py
@@ -1,12 +1,8 @@
-"""
-Modelo utilizado na classificação das imagens histológicas.
-"""
-
 from torchvision.models import (
     efficientnet_b0,
     EfficientNet_B0_Weights
 )
-from torch import nn
+from torch import nn # é o módulo de redes neurais do PyTorch
 
 from config import NUM_CLASSES, PRETRAINED
 
@@ -18,17 +14,19 @@ def create_model():
     """
 
     if PRETRAINED:
-        weights = EfficientNet_B0_Weights.DEFAULT
+        weights = EfficientNet_B0_Weights.DEFAULT # usando os pesos padrões pré-treinados no ImageNet
     else:
         weights = None
 
     model = efficientnet_b0(weights=weights)
 
-    in_features = model.classifier[1].in_features
+    in_features = model.classifier[1].in_features # obtendo o número de features de entrada da camada final
 
     model.classifier[1] = nn.Linear(
-        in_features,
-        NUM_CLASSES
-    )
+        in_features, # o número de entradas permanece o mesmo
+        NUM_CLASSES # o número de saídas é o número de classes do nosso dataset
+    ) # retorna logits
+    
+    # Como nenhuma camada é congelada temos um fine-tuning completo
 
     return model
